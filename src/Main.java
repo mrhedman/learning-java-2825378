9b43e48df3d4f47a5f0b3476389539f7f5b21539
Niclas <niclas.hedman@gmail.com>
2020-12-25 09:56:57 +0100

    La till ett Scanner objekt för att kunna tilldela GPA på en student

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
-        System.out.println(studentFirstName + " " +
-                studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update it to?");
+        Scanner scanner = new Scanner(System.in);
+
+        int age = 41;
+        double gpa;
+        char firstInitial = 'N';
+        char lastInitial = 'H';
+        boolean perfectAttendance = false;
+        String name = "Niclas Hedman";
+
+        System.out.print("Enter gpa: ");
+        gpa = scanner.nextDouble();
+        scanner.close();
+        System.out.println("Students initials are: " + firstInitial + lastInitial);
+        System.out.println("Age: " + age);
+        System.out.println("gpa: " + gpa);
+        System.out.println(perfectAttendance ? "Has been to every class" : "Has not been to every class");
+        System.out.println("Name: " + name);
     }
 }
