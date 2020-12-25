090f1b78e03bb297152030876a8a6f377bf05b54
Niclas <niclas.hedman@gmail.com>
2020-12-25 10:10:49 +0100

    uppgift 3.6

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+
+        while(isOnRepeat) {
+            System.out.println("Playing same song again");
+
+            String userInput = input.next();
+            if (userInput.equals("Yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song");
+        input.close();
     }
 }
